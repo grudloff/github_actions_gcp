650e384bab9e34b9f0e0faf6ad989c9b34990d0d
Gabriel Rudloff <gabriel.rudloff@gmail.com>
2025-01-08 00:29:42 +0100

    add experiment tracking

--- trainer/task.py
+++ trainer/task.py
@@ -1,11 +1,36 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve
+from google.cloud import aiplatform
 import joblib
 import os
+import argparse
+from datetime import datetime
+
+EXPERIMENT_NAME = "iris-experiment"
 
 def train():
+    parser = argparse.ArgumentParser(description='Train a model on the Iris dataset.')
+    parser.add_argument('--local', type=bool, required=False,
+                        help='Should be set to true if running locally.',
+                        default=False)
+    local = parser.parse_args().local
+
+    PROJECT_ID = os.getenv('PROJECT_ID')
+    LOCATION = os.getenv('LOCATION')
+
+    aiplatform.init(
+        experiment=EXPERIMENT_NAME,
+        project=PROJECT_ID,
+        location=LOCATION
+    )
+
+    # Set up experiment tracking
+    aiplatform.autolog()
+    aiplatform.start_run(run=f"sklearn-iris-run-{datetime.now().strftime('%Y%m%d%H%M%S')}")
+
+
     # Load the iris dataset
     iris = load_iris()
     X, y = iris.data, iris.target
@@ -23,12 +48,33 @@ def train():
     y_pred = clf.predict(X_test)
 
     # Save the model
-    filepath = f"/gcs/{os.environ["AIP_MODEL_DIR"]}/model_artifacts/model.bst"
+    if local:
+        MODEL_DIR = "local_model_dir"
+    else:
+        MODEL_DIR = os.getenv('AIP_MODEL_DIR')
+        MODEL_DIR = MODEL_DIR.replace('gs://', '/gcs/')
+
+    filepath = os.path.join(MODEL_DIR, 'model.joblib')
+    folder_path = os.path.dirname(filepath)
+    os.makedirs(folder_path, exist_ok=True)
     joblib.dump(clf, filepath)
 
     # Calculate the accuracy
     accuracy = accuracy_score(y_test, y_pred)
+    aiplatform.log_metrics({'accuracy': accuracy})
     print(f'Accuracy: {accuracy:.2f}')
+
+    labels = list(iris.target_names)
+    matrix = confusion_matrix(y_test, y_pred)
+    matrix = matrix.tolist()
+
+    aiplatform.log_classification_metrics(
+        labels=labels,
+        matrix=matrix
+    )
+
+    # Stop experiment run
+    aiplatform.end_run()
 
 if __name__ == '__main__':
     train()
